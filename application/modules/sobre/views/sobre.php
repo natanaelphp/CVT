e5f591e10cf0f42ae446601863afe798dc0c007f
Natanael <natanael.php@gmail.com>
2013-01-24 13:58:58 -0200

    Sobre

--- application/modules/sobre/views/sobre.php
+++ application/modules/sobre/views/sobre.php
@@ -28,12 +28,21 @@
         #creditos{
             clear: both;
         }
+        
+        a:hover{
+            text-decoration: underline;
+        }
     </style>
     <div id='dados'>
+        Sistema desenvolvido por <a target="_blank" href="http://natanael.net/">Natanael Colacino</a>
+        <?php echo br(3); ?>
         <img src="<?php echo base_url('/asset/img/sobre/responsive.png'); ?>">
-        Natanael
+        <?php echo br(2); ?>
+        <a target="_blank" href="http://natanael.net/">natanael.net</a>
     </div>
     <div id='form-contato'>
+        Entre em contato com o desenvolvedor.
+        <?php echo br(3);?>
         <?php
             echo form_open('sobre/enviaEmail');
                 echo form_label('Nome :','nome');
